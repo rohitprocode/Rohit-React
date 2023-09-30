5b9ba3a9880eba5f8425139509152927162113ef
rohitprocode <rohit990797@gmail.com>
2023-09-30 10:49:30 +0530

    Binding as Arrow function in render method

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -24,4 +24,29 @@ import React, { Component } from 'react'
 //   }
 // }
 
+//Binding as Arrow function in render method
+
+class EventBind extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         message : "Hello"
+      }
+    }
+    changeText(){
+        this.setState({
+            message : "Bye"
+        })
+    }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.message}</h1>
+        <button onClick={()=>this.changeText()} >Click</button>
+      </div>
+    )
+  }
+}
+
 export default EventBind
