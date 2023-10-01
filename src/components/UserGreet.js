f82f10835fea5daa80e92f3d8080197ed2f82afb
rohitprocode <rohit990797@gmail.com>
2023-10-01 13:16:33 +0530

    Conditional rendering - Variable  in React

--- src/components/UserGreet.js
+++ src/components/UserGreet.js
@@ -1,23 +1,44 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 
 
-//Use of IF/ELSE 
-class UserGreet extends Component{
+//Conditional Rendering : Use of IF/ELSE
+// class UserGreet extends Component{
+//     constructor(props) {
+//       super(props)
+
+//       this.state = {
+//          isLoggedIn : true
+//       }
+//     }
+
+//     render(){
+//         if(this.state.isLoggedIn){
+//             return <div>Hello Rohit</div>
+//         }else{
+//             return <div>Hello Guest</div>
+//         }
+//     }
+// }
+
+//Conditional Rendering : Use of Variable
+class UserGreet extends Component {
     constructor(props) {
-      super(props)
-    
-      this.state = {
-         isLoggedIn : true
-      }
-    }
+        super(props)
 
-    render(){
-        if(this.state.isLoggedIn){
-            return <div>Hello Rohit</div>
-        }else{
-            return <div>Hello Guest</div>
+        this.state = {
+            isLoggedIn: false
         }
     }
-}
 
+    render() {
+        let Message;
+        if (this.state.isLoggedIn) {
+            Message = <div>Hello Rohit</div>
+        } else {
+            Message = <div>Hello Guest</div>
+        }
+
+        return <div>{Message}</div>
+    }
+}
 export default UserGreet
