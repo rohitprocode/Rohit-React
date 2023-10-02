07621caa1ca62b0fe6ac58b68d9ecbc022e1b7e6
rohitprocode <rohit990797@gmail.com>
2023-10-02 16:29:21 +0530

    With Map method

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
-function NameList() {
+//List Rendering
+
+// function NameList() {
+//     const names = ['Bruce','Clark','Diana']
+//   return (
+//     <div>
+//       <h2>{names[0]}</h2>
+//       <h2>{names[1]}</h2>
+//       <h2>{names[2]}</h2>
+//     </div>
+//   )
+// }
+
+//With Map method
+
+function NameList(){
     const names = ['Bruce','Clark','Diana']
-  return (
-    <div>
-      <h2>{names[0]}</h2>
-      <h2>{names[1]}</h2>
-      <h2>{names[2]}</h2>
-    </div>
-  )
+    return(
+        <div>{
+            names.map(name => <h2>{name}</h2>)
+        }</div>
+    )
 }
 
 export default NameList
