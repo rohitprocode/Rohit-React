2460a70a36bff1edac047b091f3d7dcbdca7164c
rohitprocode <rohit990797@gmail.com>
2023-10-11 01:43:38 +0530

    Pure Component

--- src/components/PureComponent.js
+++ src/components/PureComponent.js
@@ -10,10 +10,11 @@ class PureComponent extends Component {
     }
     
   render() {
+    console.log("Re-rendered")
     return (
       <div>
         <h1>My Age is : {this.state.age}</h1>
-        <button onClick={()=>this.setState({age : this.state.age + 1})} >Update</button>
+        <button onClick={()=>this.setState({age : 1})} >Update</button>
       </div>
     )
   }
