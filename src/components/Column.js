978c58fa27218833b015be1c7b878677c61b78d7
rohitprocode <rohit990797@gmail.com>
2023-10-11 01:02:14 +0530

    Fragments

---
+++ src/components/Column.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Column() {
+    return (
+        <React.Fragment>
+            <th>Rohit Rathore</th>
+            <td>Address : 14,Jai Ambe Nagar,Sukhliya,Indore</td>
+        </React.Fragment>
+    )
+}
+
+export default Column
