34793963b76e6ebe433ce5653542ec12808fc0c6
rohitprocode <rohit990797@gmail.com>
2023-10-13 19:46:48 +0530

    refs

--- src/App.js
+++ src/App.js
@@ -23,11 +23,13 @@ import PyorComponent from './components/PureComponent';
 import MemoComponents from './components/MemoComponents';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
 function App() {
   return (
     <div className="App">
+      <FRParentInput/>
       {/* <RefsDemo/> */}
-      <FocusInput/>
+      {/* <FocusInput/> */}
       {/* <MemoComponents/> */}
       {/* <PyorComponent/> */}
       {/* <Table /> */}
